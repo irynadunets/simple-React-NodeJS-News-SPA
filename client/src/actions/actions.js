12a53b5a94483255d3c57fe578140c793dc46d3a
Iryna Dunets <idunets@yahoo.com>
2020-01-07 19:27:32 +0100

    Add files via upload

---
+++ client/src/actions/actions.js
@@ -0,0 +1,46 @@
+import actionTypes from '../constants/actionTypes';
+
+function newsItemReceived(newsItem){
+    return {
+        type: actionTypes.NEWSITEM_RECEIVED,
+        newsItem: newsItem
+    }
+}
+
+function newsReceived(news){
+    return {
+        type: actionTypes.NEWS_RECEIVED,
+        news: news
+    }
+}
+
+function newsItemLoading(){
+    return {
+        type: actionTypes.NEWSITEM_LOADING
+    }
+}
+
+//export function fetchNews(fakeNews){
+    //return dispatch => {
+       // dispatch(newsReceived(fakeNews));
+    //}
+//}
+
+export function fetchNews(){
+    return dispatch => {
+        return fetch(`/news`)
+        .then( (response) => response.json() )
+        .then( (data) => dispatch(newsReceived(data.data)))
+        .catch( (e) => console.log(e) );
+    }    
+}
+
+export function fetchNewsItem(id){
+    return dispatch => {
+        return fetch(`/news/${id}`)
+        .then( (response) => response.json() )
+        .then( (data) => dispatch(newsItemReceived(data.data)))
+        .catch( (e) => console.log(e) );
+    }    
+}
+
